d2dcf462a7e37604b6b04e22306e89d8dc4cf368
Ari Thomson <2204636@uad.ac.uk>
2025-10-20 23:26:21 +0100

    Parameterised the powerup chance

--- Breakout/GameManager.h
+++ Breakout/GameManager.h
@@ -49,6 +49,7 @@ private:
 
     static constexpr float PAUSE_TIME_BUFFER = 0.5f;
     static constexpr float POWERUP_FREQUENCY = 7.5f;    // time between minimum powerup spawn
+    static constexpr int POWERUP_CHANCE = 700.f;    
 
     Gun* gun;
 };
